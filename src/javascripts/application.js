e72a955e119ed6f32b81a421436a08ca8cdfa567
hikumho <hi.jinhu.zhang@gmail.com>
2016-10-18 23:45:48 +0800

    博客首页

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,3 +1,19 @@
-$(function(){
+/* 切换语言 */
+var $lang=0;  //0-EN 1-中文
+$(".switch-lang").click(function(){
+		if ( $lang == 0 ) {
+				// 切换到EN
+				$(this).html("中文");
+				$lang = 1;
+		} else {
+				// 切换到中文
+				$(this).html("EN");
+				$lang = 0;
+		}
+});
 
+/* 登出 */
+$(".navbar #logout").click(function(){
+	 $(".navbar .drop-nav").css("display","none");
+	 $(".navbar .login-group").css("display","");
 });
