96aa5ae949246a9b9ee152427ab9c9aede0dd36c
hikumho <hi.jinhu.zhang@gmail.com>
2016-10-21 14:04:52 +0800

    提交文章编辑区

--- src/javascripts/edit-post.js
+++ src/javascripts/edit-post.js
@@ -1,3 +1,4 @@
+/* 文章分类处理 */
 $(function() {
     /* 文章分类项删除操作 */
     function delete_category_item() {
@@ -53,4 +54,45 @@ $(function() {
     var $post_category_selected_li = $(".post-category-menu .category-item.li-active"); //当前默认已选中的分类项
     delete_category_item();  //注册事件
     select_category_item();
+});
+
+
+/* 文章编辑区样式处理： 正处于编辑区则加深边线 */
+$(function(){
+
+    var $edit_tabs = $("#edit-tabs");
+    var $edit_tabs_lia = $("#edit-tabs li a");
+    var $edit_content = $("#tab-content");
+
+    /* 在编辑区或预览区 则边线加深  */
+    /* $tab_index区分编辑区或预览区  */
+    function edit_border_deepen($tab_index){
+        $edit_tabs.addClass("edit-tabs-border-deepen");
+        $edit_content.addClass("edit-cotent-border-deepen");
+        $edit_tabs_lia.eq($tab_index).addClass("edit-tab-border-deepen");
+    }
+
+    /* 不在编辑区 边线变浅 */
+    function edit_border_clear(){
+        $edit_tabs.removeClass("edit-tabs-border-deepen");
+        $edit_content.removeClass("edit-cotent-border-deepen");
+        $edit_tabs_lia.each(function(){ $(this).removeClass("edit-tab-border-deepen"); });
+    }
+
+    
+    /* 获取编辑区的节点 */
+    $(document).click(function(e){
+        // 清除之前的边线样式
+        edit_border_clear();
+        var $edit_node = $(e.target);  // 获取当前页面的点击节点
+        if ( $edit_node.is(".edit-body-node") ) {  // 判断是否为编辑区的节点
+            var $edit_or_preview = 0;
+            if ( $edit_node.is(".edit-node") ) {
+                $edit_or_preview = 0;  //是编辑节点
+            }else {
+                $edit_or_preview = 1;  //是预览节点
+            }
+            edit_border_deepen($edit_or_preview);
+        }
+    });
 });
