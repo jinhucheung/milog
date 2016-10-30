e0cc26b982cc831fd400347ff91b094db3a59c4f
hikumho <hi.jinhu.zhang@gmail.com>
2016-10-30 23:03:06 +0800

    修改cookies[:locale]的保留时间

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   	# 设置语系
   	supported_locale = %w(zh-CN en)
   	if params[:locale] && I18n.locale_available?( params[:locale].to_sym ) && supported_locale.include?( params[:locale] )
-  	  cookies[:locale] = params[:locale]
+  	  cookies.permanent[:locale] = params[:locale]
   	end
   	I18n.locale = cookies[:locale] || I18n.default_locale
   end
