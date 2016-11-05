b5272350385a0fbdc6e5f8f1aed158673e99b531
hikumho <hi.jinhu.zhang@gmail.com>
2016-11-05 23:47:48 +0800

    完成重置密码处理

--- app/controllers/accounts/passwords_controller.rb
+++ app/controllers/accounts/passwords_controller.rb
@@ -1,4 +1,9 @@
 class Accounts::PasswordsController < ApplicationController
+  before_action :check_params, only: [:edit, :update]
+  before_action :get_user, only: [:edit, :update]
+  before_action :check_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -18,11 +23,41 @@ class Accounts::PasswordsController < ApplicationController
   end
 
   def edit
-    render 'new'
   end
 
   def update
-
+    if @user.update_attributes reset_password_params
+      @user.del_attr_digest :reset_password
+      flash[:info] = I18n.t "flash.success.reset_password"
+      redirect_to signin_path  
+    else
+      render 'edit'
+    end
   end
+
+  private
+    def reset_password_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def check_params
+      return render_404 unless params[:id] && params[:token]
+    end
+
+    def get_user
+      @user = User.find_by username: params[:id]
+    end
+
+    def check_user
+      return render_404 unless @user && @user.authenticated?(:reset_password, params[:token])
+    end
+
+    def check_expiration
+      if @user.digest_expired? :reset_password
+        flash[:warning] = I18n.t 'flash.warning.link_expired'
+        redirect_to root_path
+      end
+    end
+
 
 end
