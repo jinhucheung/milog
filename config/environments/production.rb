106207fa7d57368e1580ac8bbaf8eb038308d88f
hikumho <hi.jinhu.zhang@gmail.com>
2016-11-21 20:48:52 +0800

    修改Production环境邮箱配置

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   # Send email settings 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "http://hijinhu.me:3000" }
+  config.action_mailer.default_url_options = { host: "http://hijinhu.me" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 end
