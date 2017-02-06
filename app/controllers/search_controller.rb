84dd037eb8a4fc22de341acf154b6c18a27e3591
hikumho <hi.jinhu.zhang@gmail.com>
2017-02-06 23:39:20 +0800

    update search action, return html view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,21 +1,34 @@
 class SearchController < ApplicationController
   before_action :check_disabled_user, only: [:show]
-  before_action :check_params, only: [:show]
+  before_action :get_token
+  before_action :get_user, only: [:show]
 
   def show
     @articles = Article.search_by_token @token, user: @user
+    @article_size = @articles.size
+    respond_to do |format|
+      format.js
+      format.html { render :articles, layout: 'blog' }
+    end
   end
 
   def index
-    @articles = Article.search_by_token params[:search][:keyword]
-    render :show
+    @articles = Article.search_by_token @token
+    @article_size = @articles.size
+    respond_to do |format|
+      format.js { render :show }
+      format.html { render :articles, layout: 'community' }
+    end
   end
 
-  private 
-    def check_params
-      return render_404 unless params[:id] && params[:search] && params[:search][:keyword]
+  private
+    def get_token
+      return render_404 if (@token = params[:token]).blank?
+    end
+
+    def get_user
+      return render_404 if params[:id].blank?
       @user = User.find_by username: params[:id]
-      @token = params[:search][:keyword]
     end
 
 end
